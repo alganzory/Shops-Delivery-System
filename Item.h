cf5fb5976e2a1ae4e08c39d83001c65c2c1c1e80
Mohamed Alganzory <blogmidoelg@gmail.com>
2020-05-16 01:15:45 +0800

    updated Item class with comparator function and displaying function

--- Item.h
+++ Item.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include <string>
 #include <memory>
 
@@ -22,4 +23,7 @@ public:
 	void addQuantity(int quantity);
 	void subQuantity(int quantity);
 	std::string getName() const;
+	void display (int width1 = 30, int width2 =20, const char* append = "\n");
+	friend bool operator==(std::shared_ptr<Item> lhs, std::string itemName); // will be used in finding i
+																			  // items which are similar
 };
