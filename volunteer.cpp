f1ca47fe9e07e3f239bd6c59063b7cbb0ac99ae9
sootoh <yolok42@gmail.com>
2020-05-17 23:16:25 +0800

    fix volunteer cpp std shared ptr vector function

--- volunteer.cpp
+++ volunteer.cpp
@@ -1,29 +1,31 @@
 #include<iostream>
+#include<string>
 #include"volunteer.h"
+#include<vector>
 
 
-Volunteer::Volunteer(string username, string password) :User(username, password) {
+Volunteer::Volunteer(std::string username, std::string password) :User(username, password) {
 
 }
 
 void Volunteer::setTransport(std::string& transport) {
     transportation = transport;
-    cout << "What is your transportation? ";
+    std::cout << "What is your transportation? ";
     getline(std::cin, transport);
 }
 
 void Volunteer::setAvailableTimes() {
     int start, end;
-    cout << "Please enter your available times: ";
-    cin >> start >> end;
-    availableTimes = make_pair(start, end);
+    std::cout << "Please enter your available times: ";
+    std::cin >> start >> end;
+    availableTimes = std::make_pair(start, end);
 }
 
-string Volunteer::getTransport() const {
+std::string Volunteer::getTransport() const {
     return transportation;
 }
 
-void Volunteer::setInfo(string name, int age, double balance, Location location) {
+void Volunteer::setInfo(std::string name, int age, double balance, Location location) {
     this->name = name;
     this->age = age;
     this->balance = balance;
@@ -38,26 +40,29 @@ bool Volunteer::isAvailable(int time) {
     }
 }
 
-void Volunteer::registerToShop(std::shared_ptr<Shops> registeredShops) {
+void Volunteer::registerToShop(std::vector<std::shared_ptr<Shop>> registeredShops) {
     registeredShops.push_back(shop);
 }
 
-void Volunteer::respondToOrder(std::shared_ptr<Order> acceptedOrders, bool accept) {
+void Volunteer::respondToOrder(std::vector<std::shared_ptr<Order>> acceptedOrders, bool accept) {
     if (accept = 1) {
-        acceptedOrders.push_back(order);                                                                        //if accept will add into acceptedOrders
-        ordersToBeAccepted.erase(std::find(ordersToBeAccepted.begin(), ordersToBeAccepted.end(), order);        //then delete from list of orders to be accepted
+        acceptedOrders.push_back(order);  //if accept will add into acceptedOrders
+        orderList.erase(std::find(orderList.begin(), orderList.end(), order);        //then delete from list of orders to be accepted
     }
 }
 
-void Volunteer::deliverOrder(std::shared_ptr<Order> acceptedOrders) {
+void Volunteer::deliverOrder(std::vector<std::shared_ptr<Order>> acceptedOrders) {
     acceptedOrders.push_back(order);
-    order.erase(std::find(order.begin(), order.end(), order);
+    order.erase(std::find(order.begin(), order.end(), order));
+    order.setDeliveryStatus(true);
+
 }
 
-double Volunteer::getReward(std::vector<double> rewards) {
+std::vector<double> Volunteer::getReward(std::vector<double> rewards) {
     return rewards;
 }
-//refer to order class do we need to add delivery status?
+
+
 //operators overloading
 //friend 
 
