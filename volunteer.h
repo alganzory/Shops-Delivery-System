ffd098565c47630d7b850f4c4b0b3ca6d149a7f0
sootoh <yolok42@gmail.com>
2020-05-17 15:02:27 +0800

    fix volunteer class header sharedptr vector function

--- volunteer.h
+++ volunteer.h
@@ -2,28 +2,34 @@
 #define volunteer_H
 
 #include"User.h"
+#include<vector>
 
 class Volunteer : public User {
 private:
     string transportation;
-    pair<int hour, int min> availableTimes;
-    Shop* registeredShops[];
-    Order* acceptedOrders[];
-    double rewards[];
+    pair<int, int> availableTimes;
+
+    std::vector<std::shared_ptr<Shop>> registeredShops;
+
+    std::vector<std::shared_ptr<Order>> acceptedOrders;
+
+    std::vector<double> rewards;
+
 public:
-    Volunteer(string usernamev, string passwordv) :User(usernamev, passwordv);
+    Volunteer(string usernamev, string passwordv);
     void setTransport(string& transport);
-    void setrewards(double& rewards[]);
-    string gettransport();
-    void setInfo(string namev, int agev, double balancev, Location locationv);
-    bool IsAvailable(int hr,int m);
-    void registerToShop(Shop* shop);
-    void respondToOrder(Order* order);
-    void deliverOrder(Order* order);
-    //getReward(g &reward);
+    void setAvailableTimes();
+    string getTransport() const;
+    void setInfo(string name, int age, double balance, Location location);
+    bool isAvailable(int time);
+    void registerToShop(std::shared_ptr<Shops> registeredShops);
+    void respondToOrder(std::shared_ptr<Order> acceptedOrders, bool accept);
+    void deliverOrder(std::shared_ptr<Order> acceptedOrders);
+    double getReward(std::vector<double> rewards);
+    //refer to order class do we need to add delivery status?
     //operators overloading
     //friend 
-
 };
 
 #endif
+
