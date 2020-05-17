77742484a673065c8bd112b5cfc438557238e55c
yilin <onezero326@gmail.com>
2020-05-17 23:03:31 +0800

    std and abbreviation problem are solved.

--- location.cpp
+++ location.cpp
@@ -1,22 +1,22 @@
 #include "location.h"
 
-Location::Location(string _address)
+Location::Location(std::string address)
 {
-	address=_address;
+	this->address=address;
 }
-Location::string getAddress() const
+std::string Location::getAddress() const
 {
 	return address;
 }
-Location::pair<int,int> getGeoLocation const
+std::pair<int,int> Location::getGeoLocation const
 {
 	return geoLocation;
 }
-Location::void setAddress(string _address)
+void Location::setAddress(std::string address)
 {
-	address=_address;
+	this->address=address;
 }
-Location::void setGeoLocation(int longitude,int latitude)
+void Location::setGeoLocation(int longitude,int latitude)
 {
 	geoLocation=make_pair(longitude,latitude);
 }		
