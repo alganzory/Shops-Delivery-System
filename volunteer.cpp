78b80770dfdd53cfc611cb9c489099bed7c1b2fb
sootoh <yolok42@gmail.com>
2020-05-17 15:01:50 +0800

    fix volunteer class sharedptr vector function

--- volunteer.cpp
+++ volunteer.cpp
@@ -2,47 +2,62 @@
 #include"volunteer.h"
 
 
-Volunteer::Volunteer(string usernamev, string passwordv) :User(usernamev, passwordv) {
-        username = usernamev;
-        password = passwordv;
-    }
-void Volunteer::setTransport(string& transport) {
-        transportation = transport;
-    }
-void Volunteer::setrewards(double& rewards[]) {
-}
-string Volunteer::gettransport() {
-        return transportation;
-}
-void Volunteer::setInfo(string namev, int agev, double balancev, Location locationv) {
-        name = namev;
-        
-        age = agev;
-        
-        balance = balancev;
-        
-        location = locationv;
-        
-}
-bool Volunteer::IsAvailable(int hr,int m) {
-        bool status = true;
-        availableTimes = make_pair(hr, m);
-        //if 
-        return status;
-}
-void Volunteer::registerToShop(Shop* shop) {
-        shops = new Shop registeredShops[];
-    }
-void Volunteer::respondToOrder(Order* order) {
-        //acceptedOrders[count-1]=order;
-    }
-void Volunteer::deliverOrder(Order* order) {
-        //acceptedOrder[count-1]=order;
-        //ReadOrders();
-    }
-    //getReward(g &reward){
+Volunteer::Volunteer(string username, string password) :User(username, password) {
 
-    //}
-    //operators overloading
-    //friend 
+}
+
+void Volunteer::setTransport(std::string& transport) {
+    transportation = transport;
+    cout << "What is your transportation? ";
+    getline(std::cin, transport);
+}
+
+void Volunteer::setAvailableTimes() {
+    int start, end;
+    cout << "Please enter your available times: ";
+    cin >> start >> end;
+    availableTimes = make_pair(start, end);
+}
+
+string Volunteer::getTransport() const {
+    return transportation;
+}
+
+void Volunteer::setInfo(string name, int age, double balance, Location location) {
+    this->name = name;
+    this->age = age;
+    this->balance = balance;
+    this->location = location;
+
+}
+
+bool Volunteer::isAvailable(int time) {
+    bool available = true;
+    if (time > availableTimes.first && time < availableTimes.second) {
+        return available;
+    }
+}
+
+void Volunteer::registerToShop(std::shared_ptr<Shops> registeredShops) {
+    registeredShops.push_back(shop);
+}
+
+void Volunteer::respondToOrder(std::shared_ptr<Order> acceptedOrders, bool accept) {
+    if (accept = 1) {
+        acceptedOrders.push_back(order);                                                                        //if accept will add into acceptedOrders
+        ordersToBeAccepted.erase(std::find(ordersToBeAccepted.begin(), ordersToBeAccepted.end(), order);        //then delete from list of orders to be accepted
+    }
+}
+
+void Volunteer::deliverOrder(std::shared_ptr<Order> acceptedOrders) {
+    acceptedOrders.push_back(order);
+    order.erase(std::find(order.begin(), order.end(), order);
+}
+
+double Volunteer::getReward(std::vector<double> rewards) {
+    return rewards;
+}
+//refer to order class do we need to add delivery status?
+//operators overloading
+//friend 
 
