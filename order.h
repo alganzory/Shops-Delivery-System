f26d457d8fbd3a496ef98122d822048d7b32b8d4
sootoh <yolok42@gmail.com>
2020-05-18 23:13:55 +0800

    fix order header vector method added getDelivery

--- order.h
+++ order.h
@@ -2,31 +2,31 @@
 #define order_H
 #include <vector>
 #include"volunteer.h"
-#include"volunteer.cpp"
-//not sure
+#include"customer.h"
+#include"Shop.h"
 #include"Item.h"
-#include"Item.cpp"
 
 class Order {
 private:
 	std::vector <std::pair<std::shared_ptr<Item>, int>> items;
 	std::shared_ptr<Customer> customer;
-	std::vector <std::shared_ptr<Shop>> shop;
+	std::shared_ptr<Shop> shop;
 	double totalPrice;
 	bool paymentStatus;
-	Volunteer* delivery;
+	std::shared_ptr<Volunteer> delivery;
 	bool deliveryStatus;
 	int deliveryTime;
 
 public:
-	Order(std::shared_ptr<Customer> customer, std::vector<std::shared_ptr<Shop>> shop);
-	void addItem(std::pair<std::shared_ptr<Item>, int quantity>);
-	void removeItem(std::pair<std::shared_ptr<Item>, int quantity>);
+	Order(std::shared_ptr<Customer> customer, std::shared_ptr<Shop> shop);
+	void addItem(std::shared_ptr<Item> item, int quantity);
+	void removeItem(std::pair<std::shared_ptr<Item>, int>);
 	void setTotalPrice(double& totalPrice);
 	double getTotalPrice();
-	void setDelivery(Volunteer* volunteer, int deliveryTime);
-	void setPaymentStatus(std::shared_ptr<Customer> customer);
+	void setDelivery(std::shared_ptr<Volunteer>volunteer);
+	void setPaymentStatus(bool& paymentStatus);
 	void setDeliveryStatus(bool& deliveryStatus);
+	std::shared_ptr<Volunteer> getDelivery();
 
 	//operator overloading to add prices???
 	//deliverystatus::so tht volunteer can call and chg the status
