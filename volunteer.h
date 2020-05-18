3e17f4414eb254777698261ccb06575955b640f6
sootoh <yolok42@gmail.com>
2020-05-18 23:15:33 +0800

    fix volunteer header vector method pair

--- volunteer.h
+++ volunteer.h
@@ -1,8 +1,6 @@
 #ifndef volunteer_H
 #define volunteer_H
 
-//#include"order.h"
-//#include"order.cpp"
 #include<vector>
 
 class Volunteer : public User {
@@ -16,14 +14,14 @@ private:
 public:
     Volunteer(std::string username, std::string password);
     void setTransport(std::string& transport);
-    void setAvailableTimes();
+    void setAvailableTimes(std::pair<int, int> availableTimes);
     std::string getTransport() const;
     void setInfo(std::string name, int age, double balance, Location location);
     bool isAvailable(int time);
-    void registerToShop(std::vector<std::shared_ptr<Shop>> registeredShops);
-    void respondToOrder(std::vector<std::shared_ptr<Order>> acceptedOrders, bool accept);
-    void deliverOrder(std::vector<std::shared_ptr<Order>> acceptedOrders);
-    std::vector<double> getReward(std::vector<double> rewards);
+    void registerToShop(std::shared_ptr<Shop> newShop);
+    void respondToOrder(std::shared_ptr<Order> acceptedOrders, bool accept);
+    void deliverOrder(std::shared_ptr<Order> acceptedOrders);
+    void getReward(double reward);
 
 
     //operators overloading
