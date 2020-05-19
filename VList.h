a3f3b37f8868271c809d5116e865c2401aee85e3
sootoh <yolok42@gmail.com>
2020-05-19 16:52:33 +0800

    V_List header added string filePath

--- VList.h
+++ VList.h
@@ -2,10 +2,13 @@
 #include"volunteer.h"
 #include<iostream>
 #include<vector>
+#include<string>
+
 class V_List {
 	public:
 		static std::vector<std::shared_ptr<Volunteer>> ALL_VOLUNTEERS;
 		static std::fstream vFile;
+		static std::string filePath;
 		static int getVolunteersCount();
 		static void AddVolunteer(std::shared_ptr<Volunteer> Volunteer);
 		static void RemoveVolunteer(std::shared_ptr<Volunteer> Volunteer);
