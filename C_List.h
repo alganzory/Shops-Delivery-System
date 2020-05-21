0f954f50b8b3b4fc382e66a96d2e0a5c980e9af8
yilin <onezero326@gmail.com>
2020-05-22 01:15:22 +0800

    private declaration is added and shared pointer mistakes are corrected.

--- C_List.h
+++ C_List.h
@@ -2,16 +2,22 @@
 #include <vector>
 #include <string>
 #include <fstream> 
+#include "customer.h"
 
 class C_List
 {
-	Public:
-		std::vector <shared_ptr<Customer>> ALL_CUSTOMERS;
+	private:
+		static std::ifstream fin;
+		static std::ofstream fout;
+		std::string filePath;
+		
+	public:
+		static std::vector<std::shared_ptr<Customer>> ALL_CUSTOMERS;
 	
-	Public:
+	public:
 		static int getCustomerCount() const;
-		static void addCustomer(Customer *customer);
-		static void removeCustomer(Customer *customer);
+		static void addCustomer(std::shared_pte<Customer> customer);
+		static void removeCustomer(std::shared_pte<Customer> customer);
 		static void readCustomers();
 		static void writeCustomer();
 };
