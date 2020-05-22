9c11c5a4d77d43e6b8bc03db590c019f7f52e00a
yilin <onezero326@gmail.com>
2020-05-22 15:55:21 +0800

    fstream mistake and silly mistakes are corrected.

--- C_List.cpp
+++ C_List.cpp
@@ -1,7 +1,6 @@
 #include "C_List.h"
 
-std::ifstream C_List::fin;
-std::ofstream C_List::fout;
+std::fstream C_List::dataFile;
 std::string C_List::filePath="file.txt";
 
 static int C_List::getCustomerCount() const
@@ -9,28 +8,28 @@ static int C_List::getCustomerCount() const
 	return ALL_CUSTOMERS.size();
 }
 
-static void C_List::addCustomer(std::shared_pte<Customer> customer)
+static void C_List::addCustomer(std::shared_ptr<Customer> customer)
 {
 	ALL_CUSTOMERS.push_back(customer);
 }
 
-static void C_List::removeCustomer(std::shared_pte<Customer> customer)
+static void C_List::removeCustomer(std::shared_ptr<Customer> customer)
 {
 	auto foundCustomer=std::find(ALL_CUSTOMERS.begin(),ALL_CUSTOMERS.end(),customer);
-	ALL_CUSTOMERS.erase(i);
+	ALL_CUSTOMERS.erase(foundCustomer);
 
 }
 
 static void C_List::readCustomers()
 {
-	fin.open(filepath); 
+	dataFile.open(filepath); 
 	//input
-	fin.close();
+	dataFile.close();
 }
 
 static void C_List::writeCustomer()
 {
-	fout.open(filepath);
+	dataFile.open(filepath);
 	//output
-	fout.close();
+	dataFile.close();
 }
