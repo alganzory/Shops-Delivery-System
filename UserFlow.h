974c580f889553a4871830f5dee8c7b022cf9591
yilin <onezero326@gmail.com>
2020-05-30 01:11:29 +0800

    verifyUsername and verifyPassword function were added. Im not sure whether it can be return like this. If not i will correct it like the one i sent in whatsapp (if that one is correct)

---
+++ UserFlow.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "User.h"
+#include "C_List.h"
+#include "V_List.h"
+#include "SO_List.h"
+#include "Customer.h"
+#include "ShopOwner.h"
+#include "Volunteer.h"
+
+class UserFlow
+{
+	public:
+		void welcomeScreen();
+		std::shared_ptr<User> verifyUsername(std::string);
+		bool verifyPassword(std::shared_ptr<User>, std::string);
+		void login();
+		void signUp();
+		void exitScreen();
+};
