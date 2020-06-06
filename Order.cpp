c01781d2d43745570256e599d02f19146ee59b2b
yilin <onezero326@gmail.com>
2020-06-07 02:50:55 +0800

    orderDisplay, getOrderSize,getItem functions were created.

--- Order.cpp
+++ Order.cpp
@@ -89,6 +89,28 @@ int Order::getDeliveryTime()
 	return deliveryTime;
 }
 
+void Order::orderDisplay()
+{
+	double total=0;
+	std::cout << std::setw(4) << "No." << std::setw(20) << "Name" << std::setw(5) << "Qty" << "Price (RM)"<<std::endl;
+	for (int i = 0; i < getOrderSize(); i++)
+	{
+		std::cout << std::setw(4) << i+1 << std::setw(20) << items[i].first->getName()<< std::setw(5) << items[i].second << items[i].second*items[i].first->getPrice() << std::endl;
+		total += items[i].second * items[i].first->getPrice();
+	}
+	std::cout << "Total price (" << getOrderSize() << " items) : RM" << total << std::endl;
+}
+
+int Order::getOrderSize()
+{
+	return items.size();
+}
+
+std::pair<std::shared_ptr<Item>, int> Order::getItem(int position)
+{
+	return items.at(position);
+}
+
 //operator overloading to add prices???
 //deliverystatus::so tht volunteer can call and chg the status
 //operator overloading
