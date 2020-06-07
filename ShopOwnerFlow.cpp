1b0c815f9fef88be2f4cb984fbedc1c365d2e56d
Mohamed Alganzory <blogmidoelg@gmail.com>
2020-06-07 14:59:53 +0800

    Created myShop function

--- ShopOwnerFlow.cpp
+++ ShopOwnerFlow.cpp
@@ -20,7 +20,61 @@ void ShopOwnerFlow::addNewItem()
 
 void ShopOwnerFlow::myShop()
 {
-	std::cout << "My Shop\n";
+	while (true)
+	{
+		std::cout << currentSO->getShopName() << '\n'; //update
+		Helper::dLine();
+		int numItems = currentSO->getShopSize();
+		std::vector <std::shared_ptr <Item> > currItems = currentSO->getShop().getItems();
+		std::cout << "Number of Items: " << numItems << '\n';  //update
+		Helper::line();
+		if (numItems) {
+			std::cout << "Here is the list of items your shop:\n";
+			Helper::line(60);
+			std::cout << std::left << std::setw(33) << "Item name" << std::setw(20)
+				<< "Price" << "Quantity" << '\n';
+			Helper::line(60);
+			int counter = 0;
+
+
+			for (auto& item : currItems)
+			{
+				std::cout << ++counter << "- ";
+				(*item).display();
+			}
+
+			Helper::line(60);
+			std::cout << "Press N to add a new item,\n"
+				<< "Enter an item Number to change its info, or Press B to go back\n";
+		}
+		else
+		{
+			std::cout << "No items to show, maybe add one?\n";
+			Helper::line(60);
+			std::cout << "Press N to add a new item or Press B to go back\n";
+
+		}
+
+		Helper::line(60);
+		std::cout << "Your choice:";
+		int choice;
+		if (numItems) choice = Helper::readChoice(1, 10, "nNbB");
+		else choice = Helper::readChoice(0, 0, "nNbB");
+
+
+
+		if (choice == 'n' or choice == 'N') ShopOwnerFlow::addNewItem();
+		if (choice == 'b' or choice == 'B') break;
+
+		try {
+			ShopOwnerFlow::editItem(currItems.at(choice - 1));
+		}
+		catch (const std::out_of_range& error)
+		{
+			std::cout << "Please choose an item from the ones shown\n";
+			continue;
+		}
+	}
 }
 
 void ShopOwnerFlow::mainMenu()
