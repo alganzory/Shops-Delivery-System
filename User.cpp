391763cdc84ec80db295d12414924d46f90a42d9
yilin <onezero326@gmail.com>
2020-06-12 15:52:57 +0800

    removeOrder function were modified

--- User.cpp
+++ User.cpp
@@ -64,7 +64,7 @@ void User::addOrder(std::shared_ptr<Order> newOrder)
 	orders.push_back(newOrder);
 }
 
-void User::removeOrder(std::shared_ptr<Order> order)
+void User::removeOrder(const std::shared_ptr<Order>& order)
 {
 	orders.erase(find(orders.begin(), orders.end(), order));
 
